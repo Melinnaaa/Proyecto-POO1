97afc20a7b0a4963c8863df00f0d37e4b5361a86
Melinnaaa <arturo.reyes.c01@mail.pucv.cl>
2022-09-01 12:39:01 -0400

    El menu solo realiza operaciones del menu, mostrar y diversas funciones

--- Hospital/src/Menu.java
+++ Hospital/src/Menu.java
@@ -4,26 +4,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class Menu 
-{
-	static private BufferedReader reader;
-	
-	Menu()
-	{
-		reader = new BufferedReader(new InputStreamReader(System.in));
-	}
-	
-	// Lee por consola
-	public String getline() throws IOException
-	{
-		return reader.readLine();
-	}
-	
-	// Lee por consola y lo pasa a int
-	public int toInt() throws IOException
-	{
-		return  Integer.parseInt(getline());
-	}
-	
+{	
 	// Muestra las opciones del menu
 	public void showMenu()
 	{
@@ -37,106 +18,17 @@ public class Menu
         System.out.println("---");
 	}
 	
-	
-	// Busca a partir del nombre.
-	public void search(Hashtable <String, Paciente> patient, Paciente tmp) throws IOException
-	{	
-		// Se comprueba que el paciente exista
-		if (tmp == null) 
-		{
-			System.out.println("No se encontró el paciente ingresado.");
-			return;
-		}
-		else
-		{
-			System.out.println("Nombre: " + tmp.getNombre());
-			System.out.println("Rut: " + tmp.getRut());
-			System.out.println("Edad: " + tmp.getEdad());
-			System.out.println("Gravedad: " + tmp.getGravedad());
-		}
-	}
-	
-	public void search(Hashtable <String, Paciente> patient) throws IOException
+	// Verifica que el numero esté en el rango indicado
+	public int verifyNumber(int low, int high) throws IOException
 	{
-		System.out.println("Ingrese el rut del paciente a buscar:");
-		String rut = getline();
-		Paciente tmp =  patient.get(rut);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int n;
 		
-		// Se comprueba que el paciente exista
-		if (tmp == null) 
+		// Se verifica que se indique una pieza que exista.
+		do
 		{
-			System.out.println("No se encontró el paciente ingresado.");
-			return;
-		}
-		else
-		{
-			System.out.println("Nombre: " + tmp.getNombre());
-			System.out.println("Rut: " + tmp.getRut());
-			System.out.println("Edad: " + tmp.getEdad());
-			System.out.println("Gravedad: " + tmp.getGravedad());
-		}
-	}
-	
-	// Se encarga de ver el tipo de busqueda que se quiere realizar.
-	public void search(Hashtable <String, Paciente> patientName, Hashtable <String, Paciente> patientRut) throws IOException
-	{
-		System.out.println("---");
-		System.out.println("1. Por nombre.");
-        System.out.println("2. Por rut.");
-        System.out.println("0. Volver.");
-        System.out.println("---");
-        
-        // in es input.
-        int in = toInt();
-        Menu m = new Menu();
-    
-        switch (in)
-        {
-        	case 1:
-        	{
-        		System.out.println("Ingrese el nombre del paciente a buscar:");
-        		String name = getline();
-        		Paciente tmp =  patientName.get(name);
-        		m.search(patientName, tmp);
-        		break;
-        	}
-        	case 2:
-        	{
-        		m.search(patientRut);
-        		break;
-        	}
-        	case 0:
-        	{
-        		return;
-        	}
-        	default:
-        	{
-        		System.out.println("Ingrese una opción válida");
-        		break;
-        	}
-        	
-        }
-	}
-	
-	public void deletePatient(Hashtable <String, Paciente> patientName, Hashtable <String, Paciente> patientRut) throws IOException
-	{
-		System.out.println("Ingrese el nombre del paciente a eliminar:");
-		String in = getline();
-		Paciente tmp = patientName.get(in);
-		if (tmp == null)
-		{
-			System.out.println("El paciente que quiere eliminar no existe");
-			return;
-		}
-		else
-		{
-			patientName.remove(in);
-			patientRut.remove(tmp.getRut());
-		}
-	}
-	
-	public void showPatients(ArrayList <Paciente> patients) throws IOException
-	{
-		
+			n = Integer.parseInt(reader.readLine());
+		}while(n < low || n > high);
+		return n;
 	}
 }
