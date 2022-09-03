43ff1d1f0d236faa4f688d4db4f8977beec5ab74
Melinnaaa <arturo.reyes.c01@mail.pucv.cl>
2022-09-02 20:26:12 -0400

    Se agrega el atributo rut sin digito, se incorpora la interfaz

--- Hospital/src/Paciente.java
+++ Hospital/src/Paciente.java
@@ -2,53 +2,79 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Paciente 
+public class Paciente implements VerifyRut
 {
 	private String name;
 	private String rut;
+	private int rutNoDigit;
 	private int age;
 	private int gravedad;
 	
 	// Constructor que inicializa todas las variables con datos de entrada
-	public Paciente(String nombre, String rut, int edad, int gravedad) 
+	public Paciente(String nombre, int rut, int edad, int gravedad) 
 	{
 		this.name = nombre;
-		this.rut = rut;
+		this.rutNoDigit = rut;
 		this.age = edad;
 		this.gravedad = gravedad;
 	}
 	
-	// COnstructor default
+	// Constructor default
 	public Paciente()
 	{
 		
 	}
 
-	public String getNombre() 
-	{
-		return name;
-	}
-
-
 	public String getRut() 
 	{
 		return rut;
 	}
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
 
-
-	public int getEdad() 
+	public int getAge() 
 	{
 		return age;
 	}
 
+	public void setAge(int age) 
+	{
+		this.age = age;
+	}
+
+	//rut sin guion
+	public void setRut(int rut) 
+	{
+		this.rutNoDigit = rut;
+	}
+	
+	// Rut con guion
+	public void setRut() 
+	{
+		// Se pasa el rut sin digito a String
+		this.rut = String.valueOf(rutNoDigit);
+		// Se concatena el guión
+		this.rut = this.rut.concat("-");
+		// Se concatena el digito verificador
+		this.rut = this.rut.concat(getDigit());
+	}
+
+	public void setGravedad(int gravedad)
+	{
+		this.gravedad = gravedad;
+	}
 
 	public int getGravedad() 
 	{
 		return gravedad;
 	}
+	
 
 	// Se obtienen los datos del paciente por consola y se guardan
-	public void getPatientData() throws IOException
+	public void setPatientData() throws IOException
 	{
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -56,8 +82,8 @@ public class Paciente
         System.out.println("Ingrese el nombre del paciente:");
         name = reader.readLine();
         
-        System.out.println("Ingrese el rut del paciente:");
-        rut = reader.readLine();
+        System.out.println("Ingrese el rut del paciente, sin digito verificador:");
+        rutNoDigit = Integer.parseInt(reader.readLine());
         
         System.out.println("Ingrese la edad del paciente:");
         age = Integer.parseInt(reader.readLine());
@@ -67,12 +93,42 @@ public class Paciente
 	}
 	
 	// Muestra los datos de los pacientes.
-	public void showPatients() throws IOException
+	public void showPatientData() throws IOException
 	{
 		System.out.println("Nombre: " + name);
 		System.out.println("Edad: " + age);
 		System.out.println("Rut: " + rut);
 		System.out.println("Gravdedad: " + gravedad);
-	}
-	
+	}	
+
+	// Se obtiene el digito verificador a partir del rut
+	public String getDigit ()
+	{
+		int rut = rutNoDigit;
+	    String digit = "";
+	    int accum = 0;
+	    int cont = 2;
+	    //
+	    while (rut != 0)
+	    {
+	        int mult = (rut % 10) * cont;
+	        //
+	        accum = accum + mult;
+	        rut = Math.round(rut / 10);
+	        cont++;
+	        // Se reinicia el contador
+	        if (cont == 8)
+	        {
+	        	cont = 2;
+	        }
+	    }
+	    int res = 11 - (accum % 11);
+	    
+	    // Casos posibles al obtener el digito
+	    if (res < 10) digit = String.valueOf(res);
+	    if (res == 10) digit = "k";
+	    if (res == 11) digit = "0";
+	    
+	    return digit;
+    }
 }
