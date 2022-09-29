3f30bbd581b64ffaff809389ab1bbc821daaf647
Melinnaaa <arturo.reyes.c01@mail.pucv.cl>
2022-09-28 21:31:11 -0300

    Quizas se elimine por no utilidad

---
+++ Hospital/src/util/Menu.java
@@ -0,0 +1,19 @@
+package util;
+public class Menu 
+{	
+	// Muestra las opciones del menu
+	public void showMenu()
+	{
+		System.out.println("Bienvenido al Hospital PUCV:");
+		System.out.println("---");
+		System.out.println("1. Agregar Paciente.");
+        System.out.println("2. Eliminar Paciente.");
+        System.out.println("3. Mostrar Pacientes.");
+        System.out.println("4. Buscar paciente.");
+        System.out.println("5. Mostrar paciente por gravedad.");
+        System.out.println("6. Modificar datos paciente.");
+        System.out.println("7. Mostrar por patologia.");
+        System.out.println("0. Salir.");
+        System.out.println("---");
+	}
+}
