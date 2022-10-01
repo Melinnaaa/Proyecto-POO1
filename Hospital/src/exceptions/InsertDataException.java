aae7b91f023200c15e6431b40e501340a6c1ea59
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-10-01 20:18:29 -0300

    Se elimina por no uso

--- Hospital/src/exceptions/InsertDataException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-@SuppressWarnings("serial")
-public class InsertDataException extends Exception
-{
-	public InsertDataException()
-	{
-		super("El dato ingresado es invalido");
-	}
-}
